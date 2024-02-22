303c0e9749f5fa354462b91e6ea6f4a3576cd131
Ronak <125375100+Ronak-harbala@users.noreply.github.com>
2024-02-22 23:05:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,98 @@
+ let userScore = 0;
+ let compScore = 0;
+ let userScoreElem = document.querySelector("#user-score");
+ let compScoreElem = document.querySelector("#comp-score");
+ let compChoice = null;
+ let pagal = document.querySelector("#faltu");
+ let faltu = 0;
+ let count = 0;
+ let msg = document.querySelector("#msg");
+ let userChoiceElem = document.querySelector("#user-choice");
+ let compChoiceElem = document.querySelector("#comp-choice");
+
+
+
+ const getCompChoice = ()=>{
+    const options = ["rock","paper","scissor"];
+    let randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+ }
+
+ const drawGame = ()=>{
+    msg.innerText = "Game was draw";
+ }
+
+ const showWinner = (userWin) => {
+    if(userWin){
+        userScore++;
+        userScoreElem.innerText = userScore;
+        msg.innerText = "You Win !";
+    }else{
+        compScore++;
+        compScoreElem.innerText = compScore;
+        msg.innerText = "You lose ):";
+    }
+ } 
+
+
+ let choices = document.querySelectorAll(".choice");
+ 
+ choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        if(count >= 10){
+        pagal.classList.remove("hide");
+        faltu++;
+        }
+        
+        count++;
+        pagal.style = `color : yellow ; position : absolute ; font-weight : bold ;font-size : ${faltu}rem; top : 0;left : 20%; `;
+
+        let userChoice = choice.getAttribute("id");
+        compChoice = getCompChoice();
+        compChoiceElem.innerText = `computer chose ${compChoice}`;
+        userChoiceElem.innerText = `You chose ${userChoice}`;
+
+        playGame(userChoice);
+
+    })
+   
+ })
+
+ const playGame = (userChoice)=>{
+
+    if(userChoice === compChoice){
+        //Draw game
+        drawGame();
+     }else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            //paper , scissor
+            if(compChoice === "paper"){
+                userWin = false;
+            }else{
+                userWin = true;
+            }
+        }else if(userChoice === "paper"){
+            //rock , scissors
+            if(compChoice === "rock"){
+                userWin = true;
+            }else{
+                userWin = false;
+            }
+        }else{
+            //rock,paper
+            if(compChoice ==="rock"){
+                userWin = false;
+            }else{
+                userWin = true;
+            }
+        }
+        showWinner(userWin);
+     }
+
+     
+    
+ } 
+
+
+ 
